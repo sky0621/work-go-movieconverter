5534bd3429a7f704ae07469ec04a32dc3db28344
sky0621 <sky14work@gmail.com>
2016-10-31 06:10:27 +0900

    bugfix

--- convertinfo.go
+++ convertinfo.go
@@ -43,7 +43,7 @@ func (c *ConvertInfo) deployPath(targetFile string) string {
 
 func (c *ConvertInfo) cmdConvertVideo() string {
 	// s := "cp " + c.inputPath() + " " + c.outputPath()
-	s := "ffmpeg -i " + c.inputPath() + " -vf scale=" + c.Scale + ":-1 rotate=0 " + c.outputPath()
+	s := "ffmpeg -i " + c.inputPath() + " -vf scale=" + c.Scale + ":-1 -vf rotate=0 " + c.outputPath()
 	// log.Println("[cmdConvertVideo]", s)
 	return s
 }
